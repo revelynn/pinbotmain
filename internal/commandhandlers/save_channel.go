ea032f6304cd3be1738ae796cdd0df8a1bc4c601
Elliot Williams <elliot.sel@gmail.com>
2021-12-11 17:09:49 +0000

    chore: Debug log for saving channels to memory

--- internal/commandhandlers/save_channel.go
+++ internal/commandhandlers/save_channel.go
@@ -12,10 +12,17 @@ type SaveChannelCommand struct {
 }
 
 func SaveChannelCommandHandler(c *SaveChannelCommand, _ *discordgo.Session, log *logrus.Entry) {
+	l := log.WithFields(map[string]interface{}{
+		"channel_id":   c.Channel.ID,
+		"channel_name": c.Channel.Name,
+	})
+
 	gc, _ := storage.Guilds.LoadOrStore(c.GuildID, &storage.GuildChannels{})
 	_, err := gc.(*storage.GuildChannels).Add(c.Channel)
 	if err != nil {
-		log.WithError(err).Error("Could not add channel")
+		log.WithError(err).Error("Could not save channel")
 		return
 	}
+
+	l.Debug("Saved channel to memory")
 }
